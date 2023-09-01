03136bc2469a45a18cbf5d45a4be3aa7c9d3267f
ramshaali2 <143259804+Ramshaali234@users.noreply.github.com>
2023-09-01 23:43:18 +0500

    Aboutpage

---
+++ ABOUTPAGE/pages/blog1.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Styles1 from "../styles/blog1.module.css";
+import Search from "../Components2/blogss";
+import Events from "../Components2/Events";
+
+function Blog1() {
+    return (
+      <>
+      
+   
+        <div className={Styles1.Blog}>
+        
+
+
+
+
+<h1 className={Styles1.section29}>
+Blog
+ </h1>
+
+</div>
+
+
+
+
+
+
+
+<Search/>
+<Events/>
+
+      </>
+  );
+}
+export default Blog1;
